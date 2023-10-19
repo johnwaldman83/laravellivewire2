8c3783d008f11dc59c4e147391d38facb2fd8e08
user <user@m1cc>
2023-10-19 19:10:27 +0200

    livewire chirp - view, validate, store

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\View\View;
+
+class ChirpController extends Controller
+{
+    //
+    public function index(): View {
+
+        // return response('Hello world');
+        return view('chirps');
+    }
+
+}
--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -28,6 +28,9 @@ $logout = function () {
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('chirps')" :active="request()->routeIs('chirps')" wire:navigate>
+                        {{ __('Chirps') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -78,6 +81,9 @@ $logout = function () {
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('chirps')" :active="request()->routeIs('chirps')" wire:navigate>
+                {{ __('Chirps')}}
             </x-responsive-nav-link>
         </div>
 
